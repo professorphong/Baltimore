363ef304a78b13ba1678aaccf7df0609645d2ecb
professorphong <phong.le@goucher.edu>
2021-08-06 16:10:06 -0400

    Update TestGeo.R

--- TestGeo.R
+++ TestGeo.R
@@ -1,3 +1,8 @@
 #This script takes a baltimore address that looks like "1234 Keswick Drive", 
 #adds in the city "Baltimore" the corresponding zip code
+#longitude and latitude
 #City council district, and Neighborhood
+
+##Packages
+install.packages('zipcodeR')
+#https://www.gavinrozzi.com/post/an-r-package-for-zip-codes/
