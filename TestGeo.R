4bf60d13eb38947a7b2be952f8fff2e6c187b12b
professorphong <phong.le@goucher.edu>
2021-08-06 16:10:14 -0400

    Update TestGeo.R

--- TestGeo.R
+++ TestGeo.R
@@ -1,8 +1,17 @@
 #This script takes a baltimore address that looks like "1234 Keswick Drive", 
 #adds in the city "Baltimore" the corresponding zip code
+#Possibly census tract
 #longitude and latitude
 #City council district, and Neighborhood
 
 ##Packages
-install.packages('zipcodeR')
+#install.packages('zipcodeR')
 #https://www.gavinrozzi.com/post/an-r-package-for-zip-codes/
+
+#maybe this was a bad one. 
+
+#https://stackoverflow.com/questions/59583943/r-get-zipcode-from-just-street-name-city-and-state
+# USPS Shipping API: 
+# For Company:  Goucher College
+# Your Username is 876GOUCH1113
+# Your Password is 204SN44QV936
